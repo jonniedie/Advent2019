e53d26337a007851a9512ca746abf718ca424699
jonniedie <47193959+jonniedie@users.noreply.github.com>
2019-12-11 23:43:46 -0500

    O niiiiiiiiiiiiiiiiiiight deviiine

--- Day11/Day11.jl
+++ Day11/Day11.jl
@@ -12,7 +12,6 @@ data = read_csv("input")
 
 
 
-## Functions
 # Robot type that stores visited squares
 mutable struct Robot
     intcode::Tape
@@ -22,24 +21,29 @@ mutable struct Robot
 end
 Robot(data, start=(0,0)) = Robot(Tape(data), start, 0, Dict(start=>0))
 
-scan_color(robot::Robot) =
-    haskey(robot.visited, robot.position) ? robot.visited[robot.position] : 0
 
+
+## Functions
 # Run intcode instructions in robot until halting
-function run_intcode!(robot::Robot, color=0)
+function paint!(robot::Robot, color=0)
     # Set initial color
     robot.visited[robot.position] = color
 
     # Pointer at -2 means program has halted
-    while robot.intcode.pointer!=-2
+    while robot.intcode.pointer != -2
         color = scan_color(robot)
         color, direction = operate!(robot.intcode, color)
         robot.visited[robot.position] = color
         move!(robot, direction)
     end
 
-    return robot
+    return robot.visited
 end
+
+scan_color(robot::Robot) = haskey(robot.visited, robot.position) ?
+                           robot.visited[robot.position] :
+                           0
+
 
 function move!(robot::Robot, direction)
     turn!(robot, direction)
@@ -56,27 +60,24 @@ function go_forward!(robot::Robot)
 end
 
 # Show the grid painted by the robot
-function paint(points::Dict, sz=(-100, 100, -50, 50))
+function show_grid(points::Dict, sz=(-10, 50, -10, 5))
     minx, maxx, miny, maxy = sz
-    xs, ys = minx:maxx, miny:maxy
-    canvas = OffsetArray(zeros(Int, maxx-minx+1, maxy-miny+1), xs, ys)
-    for x in xs
-        for y in ys
-            if haskey(points, (x,y))
-                canvas[x,y] = points[(x,y)]
-            end
+    canvas = OffsetArray(zeros(Int, maxx-minx+1, maxy-miny+1), minx:maxx, miny:maxy)
+    for ((x,y), color) in points
+        if (minx ≤ x ≤ maxx) && (miny ≤ y ≤ maxy)
+            canvas[x,y] = color
         end
     end
     reverse(permutedims(collect(canvas), [2,1]), dims=1) |> spy
 end
-paint(robot::Robot, args...) = paint(robot.visited, args...)
+show_grid(robot::Robot, args...) = show_grid(robot.visited, args...)
 
 
 
 ## Part 1
 # Answer getter
 get_answer(data) = Robot(data) |> get_answer
-get_answer(robot::Robot) = run_intcode!(robot).visited |> length
+get_answer(robot::Robot) = paint!(robot) |> length
 
 # Get answer
 robot = Robot(data)
@@ -86,10 +87,7 @@ answer1 = get_answer(robot)
 
 ## Part 2
 # Answer getter
-function get_answer(robot::Robot)
-    run_intcode!(robot, 1)
-    return paint(robot, (-10, 50, -10, 5))
-end
+get_answer(robot::Robot) = paint!(robot, 1) |> show_grid
 
 robot = Robot(data)
 answer2 = get_answer(robot)
