d7842e06732d942a8209034de5224a94f8dbf98c
jonniedie <47193959+jonniedie@users.noreply.github.com>
2019-12-18 14:21:42 -0500

    Toys in every store

--- Day16/Day16.jl
+++ Day16/Day16.jl
@@ -10,61 +10,70 @@ import Base: *, ^
 ## Structs and functions
 # Stripey FFT matrix
 struct StripeyBoi
+    verbose
 end
+StripeyBoi(;verbose=false) = StripeyBoi(verbose)
 
 # StripeyBoi to an integer power
 struct PowerBoi
     power::Integer
+    verbose
 end
+PowerBoi(power; verbose=false) = PowerBoi(Power, verbose)
 
 # Raising a stripey boi to an integer power makes a power boi
-(^)(mat::StripeyBoi, int::Integer) = PowerBoi(int)
+(^)(mat::StripeyBoi, int::Integer) = PowerBoi(int, mat.verbose)
 
 # Multiplication by a vector handles getting absolute value of ones place
 function (*)(mat::StripeyBoi, vect)
     filt!(y) = filter!(x-> 0<x≤len, y)
+
     len = length(vect)
     cumuvec = cumsum(vect)
     temp = zero(vect)
+
     @threads for i in eachindex(vect)
         if i==1
-            temp[i] = sum(vect[1:4:len]) - sum(vect[3:4:len])
+            temp[i] = abs(sum(vect[1:4:len]) - sum(vect[3:4:len])) % 10
+
         else
             posidx = collect((3i-1):(4i):len-1)
             negidx = collect((i-1):(4i):len-1)
+
             posidx1 = negidx .+ i
             negidx1 = posidx .+ i
+
             endcheck = mod(div(len, i), 4) + 1
             if endcheck==2
                 posidx1[end] = len
             elseif endcheck==4
                 negidx1[end] = len
             end
+
             posidx = vcat(posidx, posidx1)
             negidx = vcat(negidx, negidx1)
+
             posidx = filt!(posidx)
             negidx = filt!(negidx)
-            temp[i] = sum(cumuvec[posidx]) - sum(cumuvec[negidx])
+
+            temp[i] = abs(sum(cumuvec[posidx]) - sum(cumuvec[negidx])) % 10
         end
     end
-    return abs.(temp) .% 10
+    return temp
 end
+
 function (*)(p::PowerBoi, vect)
-    temp = zero(vect)
-    last_temp = copy(vect)
+    temp = copy(vect)
     mat = StripeyBoi()
     for i in 1:p.power
-        temp .= mat*last_temp
-        last_temp .= temp
-        println("Iteration $i complete")
+        temp = mat*temp
+        p.verbose && println("Iteration $i complete")
     end
     return temp
 end
 
 
 
-## Setup
-# Create stripey boi
 mat = StripeyBoi()
 
 # Test
@@ -83,6 +92,7 @@ println("Part 1 answer: ", answer1)
 
 
 ## Part 2
+mat = StripeyBoi(verbose=true)
 vect = repeat(vect, 10_000)
 ans_vect = (mat^100 * vect)
 offset = vect[1:7] |> vector2int
